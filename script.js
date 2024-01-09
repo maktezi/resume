c3fa11172083ccf6fa7febad8ea9e2859b3ce494
Mark Nierras <94935785+maktezi@users.noreply.github.com>
2024-01-09 16:33:00 +0800

    uploaded

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".my-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
